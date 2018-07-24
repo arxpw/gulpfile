6e5eebf848220cdfa63ec8021769d74d7236b06b
craigboldy <craig@boldy.co.uk>
2018-07-24 20:34:55 +0100

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,17 @@ var uglify       = require('gulp-uglify');
 var rename       = require('gulp-rename');
 var sass         = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps   = require('gulp-sourcemaps');
 
 // Define the base folders
-var src  = 'assets/src';
-var dist = 'assets/dist';
+var src  = 'src';
+var dist = 'dist';
 
 // Concatenate & Minify JS
 gulp.task( 'scripts', function () {
    
     return gulp.src([
         // Uncomment the line below and change the path to the node_module you need
-        // 'node_modules/some-package/dist/file.js',
+        'node_modules/path-to-package/dist/index.js',
         src + 'js/vendor/*.js',
         src + 'js/*.js'
     ])
@@ -32,13 +31,12 @@ gulp.task( 'scripts', function () {
 gulp.task( 'sass', function () {
 
     return sass(src + '/scss/main.scss', { style: 'compressed' })
-        .pipe( sourcemaps.init() )
         .pipe( autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
+        .pipe( gulp.dest( dist + '/css' ) )
         .pipe( rename( { suffix: '.min' } ) )
-        .pipe( sourcemaps.write('.') )
         .pipe( gulp.dest( dist + '/css' ) );
 
 });
